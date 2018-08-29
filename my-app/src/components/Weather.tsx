3d03b10366d0ca6662603baa3ebeadb8701f3c80
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2018-08-30 00:27:43 +1200

    added weather icons

--- my-app/src/components/Weather.tsx
+++ my-app/src/components/Weather.tsx
@@ -1,6 +1,20 @@
 import CircularProgress from '@material-ui/core/CircularProgress';
 import deepPurple from '@material-ui/core/colors/deepPurple';
+import Typography from '@material-ui/core/Typography';
 import * as React from "react";
+import { Card, CardContent, CardHeader } from '../../node_modules/@material-ui/core';
+import sunny from '../assets/Weather/if_weather-01_1530392.png';
+import partly_cloudy from '../assets/Weather/if_weather-02_1530391.png';
+import thundershower from '../assets/Weather/if_weather-08_1530385.png';
+import hot from '../assets/Weather/if_weather-21_1530373.png';
+import cloudy from '../assets/Weather/if_weather-22_1530369.png';
+import thunderstorms from '../assets/Weather/if_weather-23_1530363.png';
+import light_snow from '../assets/Weather/if_weather-24_1530371.png';
+import snow from '../assets/Weather/if_weather-25_1530370.png';
+import wind from '../assets/Weather/if_weather-26_1530361.png';
+import foggy from '../assets/Weather/if_weather-27_1530368.png';
+import smoke from '../assets/Weather/if_weather-28_1530367.png';
+import shower from '../assets/Weather/if_weather-30_1530365.png';
 
 export default class Weather extends React.Component<any, any> {
     
@@ -40,11 +54,12 @@ export default class Weather extends React.Component<any, any> {
             return (
                 <div>
                     {this.getCurrent()}
-                    {this.getForecast(0)}
                     {this.getForecast(1)}
                     {this.getForecast(2)}
                     {this.getForecast(3)}
                     {this.getForecast(4)}
+                    {this.getForecast(5)}
+                    {this.getForecast(6)}
                 </div>
             );
         } else {
@@ -66,8 +81,8 @@ export default class Weather extends React.Component<any, any> {
         const date = this.state.forecast[0].date;
         const day = this.state.forecast[0].day;
         const temp = this.state.condition.temp;
-        const high = this.state.forecast[0].high;
-        const low = this.state.forecast[0].low;
+        const high:number = this.state.forecast[0].high;
+        const low:number = this.state.forecast[0].low;
         const text = this.state.condition.text;
         return (
             <div className="centreText">
@@ -88,43 +103,93 @@ export default class Weather extends React.Component<any, any> {
     }
 
     public getForecast(num:number) {
+        const code = this.state.forecast[num].code;
         const date = this.state.forecast[num].date;
         const day = this.state.forecast[num].day;
-        const high = this.state.forecast[num].high;
-        const low = this.state.forecast[num].low;
+        const high = Number(this.state.forecast[num].high);
+        const low = Number(this.state.forecast[num].low);
         const text = this.state.forecast[num].text;
         return (
-            <div className="centreText">
-                {date} {day}<br/>
-                high {high}&deg;F<br/>
-                low {low}&deg;F<br/>
-                {text}<br/>
-            </div>
+            <Card className="weatherCard">
+                <CardHeader
+                    title = {day}
+                    subheader = {date + ", " + text}
+                />
+                <CardContent>
+                    <Typography component="p">
+                        <img src={this.getImageAddress(code)} alt={"image not available for #"+code}/>
+                        {this.ftoC((high+low)/2).toFixed(1)}&deg;C<br/>
+                    </Typography>
+                </CardContent>
+            </Card>
         );
+    }
+
+    public getImageAddress(code:string) {
+        switch (code){
+        case '32':
+        case '33':
+        case '34':
+            return sunny;
+        case '27':
+        case '28':
+        case '29':
+        case '30':
+        case '44':
+            return partly_cloudy;
+        case '26':
+            return cloudy;
+        case '47':
+        case '45':
+            return thundershower;
+        case '36':
+            return hot;
+        case '3':
+        case '4':
+            return thunderstorms;
+        case '13':
+        case '14':
+        case '42':
+            return light_snow;
+        case '15':
+        case '16':
+        case '41':
+        case '43':
+            return snow;
+        case '20':
+            return foggy;
+        case '19':
+        case '21':
+        case '22':
+            return smoke;
+        case '23':
+        case '24':
+            return wind;
+        case '11':
+        case '12':
+        case '39':
+        case '40':
+            return shower;
+        default:
+            return '';
+        }
+    }
+
+    public ftoC(num:number) {
+        const temp = (num-32)*5/9;
+        return temp;
     }
 
     public render() {
         return (
             <div>
-                {this.load()}
-            </div>
-            /*
-            <div id="Weather">
-                <h3>Current Conditions</h3>
-                {
-                    this.state.isFetching ? <img className='loading' src='../public/images/triangle.gif' />
-                    : this.state
-                }
-                <h3>Five-Day Forecast</h3>
-                <div id="show-forecast">
-                {
-                    this.state.isFetching ? <img className='loading' src='../public/images/triangle.gif' />
-                    : <a/>
-                    })
-                }
+                <div>
+                    {this.load()}
+                </div>
+                <div>
+                    <a href="https://www.yahoo.com/?ilc=401" target="_blank"> <img src="https://poweredby.yahoo.com/purple.png" width="134" height="29"/> </a>
                 </div>
             </div>
-            */
         );
     }
 }
